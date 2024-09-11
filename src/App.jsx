947643579863443d24d000265bb6e58d82c1c90a
fipera <filipeg.bello@gmail.com>
2024-09-10 21:31:26 -0300

    commit 1

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import {Post} from "./Post"
+
+
+export function App() {
+
+  return (
+    <div>
+      <Post  
+        author="Fipao" 
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, vitae pariatur! Repudiandae dignissimos totam mollitia quos recusandae? Veritatis, totam nemo! Minima voluptatibus repellat dignissimos expedita atque veniam exercitationem repellendus nihil!"
+      />
+      <Post
+        author="John"
+        content="Outra msg"
+      />
+    </div>
+   
+  )
+}
+
+
+
