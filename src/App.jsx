0f22372a444ed67c5640cb6a0214fc76a38600ab
Filipe Bello <filipeg.bello@gmail.com>
2024-09-16 18:24:40 -0300

    a1

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,31 @@
-import {Post} from "./Post"
-
+import { Header } from './components/header'
+import {Post} from './Post'
+import './global.css'
+import styles from './App.module.css'
+import { Sidebar } from './components/Sidebar'
 
 export function App() {
 
   return (
     <div>
-      <Post  
-        author="Fipao" 
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, vitae pariatur! Repudiandae dignissimos totam mollitia quos recusandae? Veritatis, totam nemo! Minima voluptatibus repellat dignissimos expedita atque veniam exercitationem repellendus nihil!"
-      />
-      <Post
-        author="John"
-        content="Outra msg"
-      />
+      <Header/>
+
+      <div className={styles.wrapper}>
+        <aside>
+          <Sidebar />
+        </aside>
+        <main>
+          <Post
+            author="Fipao"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, vitae pariatur! Repudiandae dignissimos totam mollitia quos recusandae? Veritatis, totam nemo! Minima voluptatibus repellat dignissimos expedita atque veniam exercitationem repellendus nihil!"
+          />
+          <Post
+            author="John"
+            content="Outra msg"
+          />
+
+        </main>
+      </div>
     </div>
    
   )
